e27587a185b0226adc8698a4f1c1d817d110f8dc
syedtalhaaskari <talha.askari@logicielservice.com>
2025-04-20 13:08:41 +0500

    Add exception handling

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import csv
 
 import requests
 
-response = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+try:
+    response = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
-with open('my_data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['id', 'title', 'completed', 'userId']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(response)
+    with open('my_data.csv', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'title', 'completed', 'userId']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(response)
+except:
+    print("Failed To Fetch!")
