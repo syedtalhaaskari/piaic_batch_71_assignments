0b7615e9249a021ccfa5eda0fab5975ddb34e20d
syedtalhaaskari <talha.askari@logicielservice.com>
2025-04-20 13:06:54 +0500

    Create a program which fetch from api and write data in a csv file

---
+++ main.py
@@ -0,0 +1,11 @@
+import csv
+
+import requests
+
+response = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+with open('my_data.csv', 'w', newline='') as csvfile:
+    fieldnames = ['id', 'title', 'completed', 'userId']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(response)
